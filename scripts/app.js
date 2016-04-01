e0f661ac155b48151b28a63e95fa6ad056495fe6
Pau Torres <ptmonlinework@gmail.com>
2016-04-01 16:11:09 +0200

    Added more comments

--- scripts/app.js
+++ scripts/app.js
@@ -6,33 +6,41 @@
       var MobileApp = {};
 
       MobileApp.profileApp = function(){
+        //add class to container and button on add to friend btn
         $('.btn-text').on('click', function(){
           $('.container').toggleClass('touched');
         })
       }
 
       MobileApp.chatSession = function(){
+        var randomMessage = Math.floor((Math.random() * 3)),
+            message,
+            archiveMessages,
+            newMessage,
+            textMessage = $('.text-message'),
+            onlineChat = $('.online-chat');
+
+        //random entries from Sophie + online status
         var messagesObject = {
           "Messages": ["It has survived not only five centuries, but also the leap into electronic.","Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.","It is a long established fact that a reader will be distracted by the readable content."],
           "online": true
         }
-        var randomMessage = Math.floor((Math.random() * 3));
-        var message;
 
-        //store random text from second user
+        //check, generate and store online atatus and random text from Sophie
         if(sessionStorage.getItem("stored_messages_receiver") !== null){
           message = JSON.parse(sessionStorage.getItem("stored_messages_receiver"))
-          message.online === true ? $('.online-chat').addClass('online') : $('.online-chat').removeClass('online');
-          $('.text-message').append(message.message)
+          //check if Sophie is online
+          message.online === true ? $(onlineChat).addClass('online') : $(onlineChat).removeClass('online');
+          $(textMessage).append(message.message)
         }else{
           message = messagesObject.Messages[randomMessage];
           window.sessionStorage.setItem('stored_messages_receiver', JSON.stringify({"message": message, "online": messagesObject.online}))
-          $('.text-message').append(message)
+          $(textMessage).append(message)
         }
 
-        var archiveMessages;
-        var newMessage;
 
+
+        //create object with template for sender
         var NewMessage = function(message){
           this.message = message;
           this.template = '<div class="sender-chat row">'+
@@ -58,11 +66,13 @@
               return false;
             }else{
               archiveMessages.push(newMessage);
+              //add message to session storage
               window.sessionStorage.setItem('stored_messages', JSON.stringify(archiveMessages))
             }
 
         })
       }
+
       MobileApp.init = function(){
         MobileApp.profileApp();
         MobileApp.chatSession();
