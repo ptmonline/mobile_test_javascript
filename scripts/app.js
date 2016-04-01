b0a8c4bb9b9226a2c2e82c273c8cf7aa039eef27
Pau Torres <ptmonlinework@gmail.com>
2016-04-01 08:49:42 +0200

    fixed styles

--- scripts/app.js
+++ scripts/app.js
@@ -13,7 +13,7 @@
         var randomMessage = Math.floor((Math.random() * 3));
         var message;
 
-
+        //store random text from second user
         if(sessionStorage.getItem("stored_messages_receiver") !== null){
           message = JSON.parse(sessionStorage.getItem("stored_messages_receiver"))
           $('.text-message').append(message)
@@ -30,12 +30,16 @@
         sessionStorage.getItem("stored_messages") !== null ? archiveMessages = JSON.parse(sessionStorage.getItem("stored_messages")) : archiveMessages=[];
 
         //if session storage nt null, populate chat page
+        var NewMessage = function(message){
+          this.message = message;
+          this.template = '<div class="sender-chat row">'+
+          '<div class="profile-pic column column-2"><a ng-href="index.html">&nbsp;</a></div>'+
+          '<div class="text-message column column-9  col-span-1-right"><span class="pin">&nbsp;</span>'+this.message+'</div></div>';
+        }
         if(archiveMessages !== null){
           $.each(archiveMessages, function(index, value){
-              var template = '<div class="sender-chat row">'+
-              '<div class="profile-pic column column-2"><a ng-href="index.html">&nbsp;</a></div>'+
-              '<div class="text-message column column-9  col-span-1-right"><span class="pin">&nbsp;</span>'+value+'</div></div>';
-              $('.sender-box').append(template)
+              var newmessage = new NewMessage(value);
+              $('.sender-box').append(newmessage.template);
           })
         }
 
